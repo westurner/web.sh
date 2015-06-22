8e33773af188f621d027fe68886511236ed78149
Wes Turner <wes@wrd.nu>
2015-06-22 06:09:36 -0500

    RLS: setup.py, __init__.py: __version = '0.1.4'

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = [
 test_requirements = [
 ]
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 description = (
     """web.sh is a cross-platform script (web) and API for launching a browser"""
     """(with x-www-browser (Linux, Unix), open (OSX), start (Windows),"""
